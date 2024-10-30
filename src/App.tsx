b00cee6db091456bdbb3dab338a557b9ce8391b0
davide <info@davideravasi.com>
2024-10-30 13:51:21 +0000

    feat: added better comments

--- src/App.tsx
+++ src/App.tsx
@@ -11,22 +11,30 @@ const posts = [
 function App() {
   const queryClient = useQueryClient();
 
-  // how to create the query key fr this kings of endpoint?
+  // how to create the query key for this kinds of endpoint?
   // /posts -> ["posts"]
   // /posts/1 -> ["posts", post.id]
   // /posts?authorId=1 -> ["posts", { authorId: 1 }]
   // /posts/2/comments -> ["posts", post.id, "comments"]
+  // the key has to be unique for this query
   const postsQuery = useQuery(
     {
       queryKey: ["posts"],
       queryFn: (obj) => {
-        // the query obj returns a set of value useful as a qeryKey, the params
-        console.log(obj);
+        // the query obj returns a set of value useful as a queryKey, 
+        // - the queryKey // it's important because you can have all info you pass as parameters (see example before)
+        //                   in the queryKey we have all the parameters sended
+        // - meta 
+        // - signal 
+        // in this function we also have .error object that is super important to have all errors related to the call - fetch method doesn't manage all the errors, we have to add it manually
+        console.log("ob", obj);
+
         return wait(1000).then(() => [...posts])
       }
     }
   )
 
+  // this is to create mutations --> change somthing in the db :)
   const mutationQuery = useMutation({
     mutationFn: (title: string) => {
       return wait(1000).then(() => posts.push({ id: parseInt(crypto.randomUUID()), title })
